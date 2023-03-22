0e9adb3481ce34470f4b237ec0fb3a9bb7aabc30
Jose Ignacio Viola <steephenamadeus@gmail.com>
2023-03-21 22:12:44 -0300

    primer commit

---
+++ Guia3EstrControl/ParImpar.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ParImpar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numero;
+
+        try {
+            System.out.print("Ingrese un número entero: ");
+            numero = input.nextInt();
+
+            if (numero % 2 == 0) {
+                System.out.println(numero + " es un número par.");
+            } else {
+                System.out.println(numero + " es un número impar.");
+            }
+        } finally {
+            input.close();
+        }
+    }
+}
---
+++ Guia3EstrControl/RS232.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class RS232 {
+    public static void main(String[] args) {
+        int correctas = 0;
+        int incorrectas = 0;
+        String input = "";
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.print("Ingrese una cadena (FDE para finalizar): ");
+                input = scanner.nextLine();
+                if (input.equals("&&&&&")) {
+                    break;
+                }
+                if (input.length() == 5 && input.charAt(0) == 'X' && input.charAt(4) == 'O') {
+                    correctas++;
+                } else {
+                    incorrectas++;
+                }
+            }
+        }
+
+        System.out.println("\n--- Informe ---");
+        System.out.println("Lecturas correctas: " + correctas);
+        System.out.println("Lecturas incorrectas: " + incorrectas);
+    }
+}
